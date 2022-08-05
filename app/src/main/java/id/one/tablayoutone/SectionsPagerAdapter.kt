ef68887825e751563980dafda3e4dd6b8af0717b
Arwan Prianto Mangidi <arwanpriantomangidi@gmail.com>
2022-08-05 21:38:42 +0800

    [UPDATE] add SectionsPagerAdapter for ViewPager in TabLayout

---
+++ app/src/main/java/id/one/tablayoutone/SectionsPagerAdapter.kt
@@ -0,0 +1,21 @@
+package id.one.tablayoutone
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionsPagerAdapter(activity: AppCompatActivity): FragmentStateAdapter(activity) {
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when(position){
+            1 -> fragment = HomeFragment()
+            2 -> fragment = ProfileFragment()
+        }
+
+        return fragment as Fragment
+    }
+
+    override fun getItemCount(): Int {
+        return 2
+    }
+}
